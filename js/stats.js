b3500c77ed4a1a7644cda8005d4d115d69940aa0
Zac McCormick <zac.mccormick@gmail.com>
2013-01-21 21:03:38 -0500

    Add number of edits in last 24 hours

--- js/stats.js
+++ js/stats.js
@@ -3,14 +3,48 @@ window.onload = function() {
 
   $.get(url, function(json) {
     if (json) {
-      $('#total-edits').html('<table><tr><td class="left">Total Edits</td><td class="right"><strong>' + json.total_edits + '</strong></td></tr><tr><td class="left">Active Users</td><td class="right"><strong>' + json.top_users.length + '</strong></td></tr><tr><td></td><td class="return"><a href="/">&laquo; Return Home</a></td></tr></table>');
+      numberOfEditsInLast24Hours = 0;
 
-      recent_html = '';
+      var now = moment(new Date);
+
+      var recent_html = '';
+
       _.each(json.recent_edits, function(edit) {
-        recent_html += '<div class="edit"><a href="http://www.openstreetmap.org/browse/changeset/' + edit.id + '" class="recent-edit">' + edit.name + ' (' + edit.tags.comment + ')</a> <span class="date">' + moment(edit.date).fromNow() + '</span></div>';
+        var editDate = moment(edit.date);
+
+        if (now.diff(editDate, 'minutes') < 1440)
+          numberOfEditsInLast24Hours++;
+
+        var changesetURL = 'http://www.openstreetmap.org/browse/changeset/' + edit.id;
+
+        recent_html += '<div class="edit">' +
+                       '  <a href="' + changesetURL + '" class="recent-edit">' + edit.name + ' (' + edit.tags.comment + ')</a> ' +
+                       '  <span class="date">' + editDate.fromNow() + '</span>' +
+                       '</div>';
       });
 
       $('#recent-edits').html(recent_html);
+
+      $('#total-edits').html(
+        '<table>' +
+        '  <tr>' +
+        '    <td class="left">Total Edits</td>' +
+        '    <td class="right"><strong>' + json.total_edits + '</strong></td>' +
+        '  </tr>' +
+        '  <tr>' +
+        '    <td class="left">Active Users</td>' +
+        '    <td class="right"><strong>' + json.top_users.length + '</strong></td>' +
+        '  </tr>' +
+        '  <tr>' +
+        '    <td class="left">Edits in Last 24 Hours</td>' +
+        '    <td class="right"><strong>' + numberOfEditsInLast24Hours + '</strong></td>' +
+        '  </tr>' +
+        '  <tr>' +
+        '    <td></td>' +
+        '    <td class="return"><a href="/">&laquo; Return Home</a></td>' +
+        '  </tr>' +
+        '</table>'
+      );
 
       top_users = '';
       _.each(json.top_users, function(user, index) {
@@ -18,8 +52,6 @@ window.onload = function() {
       });
 
       $('#top-users').html(top_users);
-
-      // $('.page-title').html('Pushpin OSM - ' + json.elements.length + ' points');
     }
   });
 }
